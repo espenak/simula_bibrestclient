61381b1139901c14430896fc8cbfc4fa1099b792
Espen A. Kristiansen <post@espenak.net>
2012-06-25 14:29:15 +0200

    defaults: Use plonerest.simula.no as default folder_url.

--- simula_bibrestclient/defaults.py
+++ simula_bibrestclient/defaults.py
@@ -1,3 +1,4 @@
 #: URL of the bibliography folder.
-folderurl = 'https://simula.no/publications/'
+#folderurl = 'https://simula.no/publications/'
+folderurl = 'http://plonerest.simula.no/publications/'
 #folderurl = 'http://localhost:8080/simula/publications/'
