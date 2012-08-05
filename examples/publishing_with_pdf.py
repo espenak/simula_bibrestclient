080d5e3d8a8e9a6ada4809861d877ee2fba5ee52
Espen A. Kristiansen <post@espenak.net>
2012-08-05 16:33:20 +0200

    examples.publishing_with_pdf: Example for konsensus and PDF.

---
+++ examples/publishing_with_pdf.py
@@ -0,0 +1,49 @@
+from simula_bibrestclient.client import BibFolder, BibItem
+from getpass import getpass
+from pprint import pprint
+
+
+# Setup our login credentials.
+auth = dict(username='griff', password='Simula123')
+folder = BibFolder(decode_output=True, **auth)
+exampleitemid = auth['username'] + '-pubexample'
+
+
+# Create the item (it will be private by default)
+create_response = folder.create_item(exampleitemid, 'ArticleReference',
+                                     title='Example',
+                                     publication_year='2012',
+                                     authors=[{'firstnames': 'Hans Petter',
+                                               'lastname': 'Langtangen',
+                                               'username': 'hpl'},
+                                              {'username': 'griff',
+                                               'firstnames': 'Carsten',
+                                               'lastname': 'Griwodz'}])
+print 'Created {title}'.format(**create_response['attributes'])
+print '   View it here: {url}'.format(**create_response)
+print 'The current portal_state is "{portal_state}", and the next possible states are: '.format(**create_response)
+pprint(create_response['portal_state_transitions'])
+
+
+## TODO: Upload PDF
+
+
+# Get an API for the created item
+itemid = create_response['attributes']['id']
+exampleitem = BibItem(itemid, decode_output=True, **auth)
+portal_type = create_response['portal_type']
+
+# Update portal_state to ``submit``, which we can see from the ``title``
+# attribute of ``portal_state_transitions`` is "Request approval from co-authors".
+update_response = exampleitem.update(portal_type, portal_state='submit')
+print 'Updated', update_response['attributes']['id']
+print 'The current portal_state is "{portal_state}", and the next possible states are: '.format(**create_response)
+pprint(create_response['portal_state_transitions'])
+
+
+# When all co-authors have approved the publication, the publication will
+# change state to ``published``.
+
+# Open a browser "confirm delete" view to delete the item we created
+print 'Opening {delete_url} in your browser...'.format(delete_url=exampleitem.get_browser_deleteurl())
+exampleitem.browser_confirm_delete()
